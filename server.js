489f79df3eff36c569aae26003db4651518a695a
vipinsingha001-netizen <vipinsingha001@gmail.com>
2026-02-09 13:28:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://s-admin-h1g0.onrender.com","https://www.s-admin-h1g0.onrender.com"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
